ccb39f565a9631fb72a21216a6ec6b8afa18eea8
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-05-04 03:55:00 +0200

    init RSA

---
+++ RSA/Program.cs
@@ -0,0 +1,24 @@
+﻿// toworzenie klucza
+using System.Numerics;
+
+Random random = new Random();
+
+int p = 7;
+int q = 13;
+BigInteger n = p*q;
+BigInteger e = random.Next(1,Euler(p, q));
+
+
+
+ Console.WriteLine($"p: {p}");
+ Console.WriteLine($"q: {q}");
+ Console.WriteLine($"n: {n}");
+
+
+
+ int Euler(int p, int q){
+    return (p-1)*(q-1);
+ }
+
+
+
