372ed84ed0569ad6133eee1f7a10963c968519f3
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-05-05 22:11:57 +0200

    CaesarCipher update

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class CaesarCipher
+{
+    static string Encrypt(string text, int shift)
+    {
+        string result = "";
+
+        foreach (char ch in text)
+        {
+            if (char.IsLetter(ch))
+            {
+                char offset = char.IsUpper(ch) ? 'A' : 'a';
+                result += (char)((((ch + shift) - offset) % 26) + offset);
+            }
+            else if (char.IsNumber(ch))
+            {
+                result += ch % 9;
+            }
+            else
+            {
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+
+    static string Decrypt(string text, int shift)
+    {
+        return Encrypt(text, 26 - shift);
+    }
+
+    static void Main()
+    {
+        string message = "123 Intruz";
+        Console.WriteLine("message: " + message);
+        int shift = 3;
+
+        string encrypted = Encrypt(message, shift);
+        Console.WriteLine("encrypted: " + encrypted);
+
+        string decrypted = Decrypt(encrypted, shift);
+        Console.WriteLine("decrypted: " + decrypted);
+    }
+}
