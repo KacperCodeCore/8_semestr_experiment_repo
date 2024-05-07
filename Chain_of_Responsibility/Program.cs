c1d7c3a54c5c3201a765f9e3879960b37c7878d6
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2024-05-07 21:44:23 +0200

    Chain_of_Responsibility update

---
+++ Chain_of_Responsibility/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ChainOfResponsibilityPattern
+{
+    public abstract class Handler
+    {
+        protected Handler _nextHandler;
+
+        public Handler SetNext(Handler handler)
+        {
+            _nextHandler = handler;
+            return handler;
+        }
+
+        public abstract void HandleRequest(string request);
+    }
+
+    public class SaleHandler : Handler
+    {
+        public override void HandleRequest(string request)
+        {
+            if (request == "sprzedaż")
+            {
+                Console.WriteLine("Sprzedano produkt.");
+            }
+            else
+            {
+                _nextHandler?.HandleRequest(request);
+            }
+        }
+    }
+
+    public class InternetHandler : Handler
+    {
+        public override void HandleRequest(string request)
+        {
+            if (request == "internet")
+            {
+                Console.WriteLine("Naprawiono problem z internetem.");
+            }
+            else
+            {
+                _nextHandler?.HandleRequest(request);
+            }
+        }
+    }
+
+    // Konkretny handler dla obsługi telefonu
+    public class PhoneHandler : Handler
+    {
+        public override void HandleRequest(string request)
+        {
+            if (request == "telefon")
+            {
+                Console.WriteLine("Naprawiono problem z telefonem.");
+            }
+            else
+            {
+                _nextHandler?.HandleRequest(request);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzenie łańcucha obsługi
+            var saleHandler = new SaleHandler();
+            var internetHandler = new InternetHandler();
+            var phoneHandler = new PhoneHandler();
+
+            saleHandler.SetNext(internetHandler).SetNext(phoneHandler);
+
+
+            saleHandler.HandleRequest("telefon");
+            saleHandler.HandleRequest("internet");
+            saleHandler.HandleRequest("sprzedaż");
+            saleHandler.HandleRequest("inny problem");
+
+        }
+    }
+}
+
